e87ca7a2a827b1af83cf882b7787e49497fb1377
Shubham Kumar <55661658+ShubhamxKumar@users.noreply.github.com>
2023-06-21 21:57:49 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <h1
+        style={{ fontFamily: "bolder", fontSize: "6rem", textAlign: "center" }}
+      >
+        Coming Soon <br />  -Metax{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
